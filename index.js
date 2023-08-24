5816981c2e11b2b3c841efad97e75d6ad432c199
ibrahim <ibrahimrabbihere@gmail.com>
2023-08-24 19:02:57 +0530

    payment system implement done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
+const stripe = require('stripe')("sk_test_51NFaHHHYxG7WJPCTo6DyF8n9Ty7LHso58T2LKEWbMN1RnwDs6Vdb8c1AIEk6ywGP4JAayNmD8PMlNtmwQBIsvcjK00SvyfXze0")
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -21,9 +22,17 @@ const client = new MongoClient(uri, {
 });
 
 const locationCollection = client.db('task').collection('location')
+const districCollection = client.db('task').collection('districAndKm')
+const orderHisCollection = client.db('task').collection('history')
+
 async function run() {
     try {
         await client.connect();
+
+        app.get('/distric', async (req, res) => {
+            const data = await districCollection.find().toArray()
+            res.send(data)
+        })
 
         app.post('/location', async (req, res) => {
             const data = await locationCollection.insertOne(req.body)
@@ -58,6 +67,18 @@ async function run() {
                     $set: { image: data.image },
                 };
             }
+
+            if (data.date) {
+                updateDoc = {
+                    $set: { date: data.date },
+                };
+            }
+
+            if (data.floor) {
+                updateDoc = {
+                    $set: { floor: data.floor },
+                };
+            }
            
            
             const final = await locationCollection.updateOne(id, updateDoc, option)
@@ -65,10 +86,39 @@ async function run() {
         })
 
         app.get('/location/:id', async (req, res) => {
-            const query = { id : req.params.id }
+            const query = { _id : new ObjectId(req.params.id) }
             const data = await locationCollection.findOne(query)
             res.send(data)
         })
+
+
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const { subTotal } = req.body;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: subTotal * 100,
+                currency: "bdt",
+                payment_method_types: ["card"],
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+
+
+
+
+        app.post("/summery", async (req, res) => {
+            const data = req.body;
+
+            //const updatedClass = await panndingCollaction.updateOne(classId, updateDoc, options);
+            const result = await orderHisCollection.insertOne(data);
+            //const deleted = await classSelectCollaction.deleteOne(id);
+            res.send(result);
+        });
     }
     finally {
 
